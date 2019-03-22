14c01498493027fe6bbf455cf874eabf6a69bb70
Crepiks <sayazhan.onlassyn@mail.ru>
2019-03-22 10:46:31 +0600

    Добавил комментарии к js

--- corporate/assets/js/video_button.js
+++ corporate/assets/js/video_button.js
@@ -1,8 +1,10 @@
 var video = document.getElementById("myVideo");
 
 // Get the button
+// Критическая ошибка: стоит дать осмысленное имя кнопке
 var btn = document.getElementById("myBtn");
 
+// Критическая ошибка: Часто в видео называют функции типа myFunction или просто Function, следует давать осмысленные имена функциям. Если проект разрастется, а это происходит не заметно, будет сложно разобраться за что отвечает эта функция 
 // Pause and play the video, and change the button text
 function myFunction() {
   if (video.paused) {
